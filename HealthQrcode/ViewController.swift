a767a86696db316684d5b1203d0eb20416681ef2
ame017 <ame017@foxmail.com>
2021-01-31 17:31:32 +0800

    1.31

--- HealthQrcode/ViewController.swift
+++ HealthQrcode/ViewController.swift
@@ -10,27 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var moreButton: UIButton!
-    @IBOutlet weak var qrImageView: UIImageView!
-    @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var moreControl: UIControl!
+    @IBOutlet weak var exitControl: UIControl!
     
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var qrImageView: UIImageView!
+    @IBOutlet weak var refreshControl: UIControl!
+
+    
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    
     
     lazy var dateFormatter : DateFormatter = {
         let object = DateFormatter()
-        object.dateFormat = "M月dd日"
+        object.dateFormat = "yyyy-MM-dd hh:mm:ss"
         return object
     }()
-    lazy var timeFormatter : DateFormatter = {
-        let object = DateFormatter()
-        object.dateFormat = "hh:mm:ss"
-        return object
-    }()
+
     var timer: Timer?
     var imageNumber = 0
 
@@ -45,39 +43,56 @@ class ViewController: UIViewController {
     //MARK: - makeUI
     private func makeUI() {
         self.backButton.setEnlargeEdge(10)
-        self.moreButton.setEnlargeEdge(15)
-        self.refreshButton.setEnlargeEdge(5)
-        self.backButton.setEnlargeEdge(10)
-        self.goButton.setEnlargeEdge(10)
+        self.refreshControl.setEnlargeEdge(10)
         loadName()
-        updateDateAndTime()
+        updateTime()
     }
     private func loadName(){
-        let name = UserDefaults.standard.string(forKey: "_user_name")
-        self.nameLabel.text = name ?? "请点击右上...输入姓名"
+        var name = UserDefaults.standard.string(forKey: "_user_name")
+        let card = UserDefaults.standard.string(forKey: "_user_id_card")
+        let hasRun = UserDefaults.standard.bool(forKey: "_has_run")
+        //由于版本迭代问题,初次运行清除名字
+        if !hasRun {
+            name = nil
+            UserDefaults.standard.setValue(nil, forKey: "_user_name")
+            UserDefaults.standard.setValue(1, forKey: "_has_run")
+        }
+        self.nameLabel.text = name ?? "点击右上角输入姓名"
+        self.idLabel.text = card ?? "点击右上角输入身份证号"
     }
+    
     private func updateTime(){
         let dateNow = Date.init()
-        let timeString = self.timeFormatter.string(from: dateNow)
+        let timeString = self.dateFormatter.string(from: dateNow)
         self.timeLabel.text = timeString
     }
-    private func updateDateAndTime(){
-        let dateNow = Date.init()
-        let dateString = self.dateFormatter.string(from: dateNow)
-        self.dateLabel.text = dateString
-        let timeString = self.timeFormatter.string(from: dateNow)
-        self.timeLabel.text = timeString
-    }
+    
+    
     //MARK: - button
-    @IBAction func moreButtonClick(_ sender: Any) {
-        let alert = UIAlertController.init(title: "FBI WORNING", message: "你叫啥?(自己输入星号)", preferredStyle: UIAlertController.Style.alert)
+    
+    @IBAction func getUserInfoButtonClick(_ sender: Any) {
+        let alert = UIAlertController.init(title: "FBI WORNING", message: "你叫啥?(不用加星号)", preferredStyle: UIAlertController.Style.alert)
         var tf = UITextField.init()
         alert.addTextField { (textField) in
             tf = textField
         }
         alert.addAction(UIAlertAction.init(title: "确定", style: UIAlertAction.Style.default, handler: { (alertAction) in
-            self.nameLabel.text = tf.text
-            UserDefaults.standard.set(tf.text, forKey: "_user_name")
+            self.nameLabel.text = "*".appending(tf.text!.suffix(tf.text!.count-1))
+            UserDefaults.standard.set(self.nameLabel.text, forKey: "_user_name")
+            //弹窗结束后立即弹窗输入身份证号
+            let alertCard = UIAlertController.init(title: "FBI WORNING", message: "身份证号!(不用加星号)", preferredStyle: UIAlertController.Style.alert)
+            var tfCard = UITextField.init()
+            tfCard.keyboardType = UIKeyboardType.asciiCapableNumberPad
+            alertCard.addTextField { (textField) in
+                tfCard = textField
+            }
+            alertCard.addAction(UIAlertAction.init(title: "确定", style: UIAlertAction.Style.default, handler: { (alertAction) in
+                self.idLabel.text = tfCard.text!.prefix(4)+"**********"+tfCard.text!.suffix(4)
+                UserDefaults.standard.set(self.idLabel.text, forKey: "_user_id_card")
+            }))
+            self.present(alertCard, animated: true) {
+                
+            }
         }))
         alert.addAction(UIAlertAction.init(title: "拉倒", style: UIAlertAction.Style.cancel, handler: { (alertAction) in
             
@@ -104,6 +119,21 @@ class ViewController: UIViewController {
     }
     @IBAction func exitButtonClick(_ sender: Any) {
         exit(0)
+    }
+    @IBAction func alipayButtonClick(_ sender: Any) {
+        UIApplication.shared.open(URL.init(string: "alipayqr://platformapi/startapp?saId=200011235")!, options: [:]) { (completion) in
+            
+        }
+    }
+    @IBAction func yunshanfuButtonClick(_ sender: Any) {
+        UIApplication.shared.open(URL.init(string: "upwallet://rn/rnhtmlridingcode")!, options: [:]) { (completion) in
+    
+        }
+    }
+    @IBAction func shengjingtongButtonClick(_ sender: Any) {
+        UIApplication.shared.open(URL.init(string: "SYSubWay://")!, options: [:]) { (completion) in
+            
+        }
     }
     
 }
